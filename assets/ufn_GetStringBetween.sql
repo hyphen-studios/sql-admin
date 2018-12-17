49bb379f6f10c212d6f0ac88f88eeb16ec041748
hyphen-studios <45641620+hyphen-studios@users.noreply.github.com>
2018-12-17 14:10:23 -0800

    Update ufn_GetStringBetween.sql

--- assets/ufn_GetStringBetween.sql
+++ assets/ufn_GetStringBetween.sql
@@ -2,12 +2,15 @@ CREATE FUNCTION ufn_GetStringBetween (@str varchar(500), @str1 varchar(30), @str
 RETURNS varchar(200)
 AS
 /******************************************************************************
-*  Stored Procedure Name: sp_CheckDBIntegrity
+*  Stored Procedure Name: ufn_GetStringBetween
 *  Input Parameters: none
-*  Use Case: exec sp_CheckDBIntegrity
+*  Use Case: select ufn_GetStringBetween('StringtoSearch', '1stStringPattern', '2ndStringPattern')
+*  select ufn_GetStringBetween('.....Str1ccccccStr2......', 'Str1', 'Str2')
+*  Results --> cccccc
+*
 *  Description: Funciton pulled from sqltips.com article - https://www.mssqltips.com/sqlservertip/4626/script-to-quickly-find-sql-server-dbcc-checkdb-errors/
 *  The function takes two strings such 'Str1' and 'Str2' and returns anything in the middle of these strings.
-*  For 
+*
 *  History:
 *  Date:		Action:								Developer: 
 *  2017-02-08	Initial version						SQL Tips - Eli Leiba
