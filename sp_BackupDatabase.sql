d07d53a3e85dcceaf2ddcd091cb1d4d186a0f146
hyphen-studios <45641620+hyphen-studios@users.noreply.github.com>
2019-03-15 12:14:08 -0700

    Create sp_BackupDatabase.sql

---
+++ sp_BackupDatabase.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE [dbo].[sp_BackupDatabase] @DBName NVARCHAR(255), @FileLocation NVARCHAR(255), @BackupFile NVARCHAR(255)  
+as
+/******************************************************************************
+*  Stored Procedure Name: sp_BackupDatabase
+*  Input Parameters: none
+*  Use Case: EXEC [P_BACKUP_DATABASE] @DBName='EDW', @FileLocation='\\sharename\foldername\ or C:\Folder Location', @BackupFile='edw-bu-20170110.bak'
+*  ** NOTE ** The SQL Engine service accounts needs administrator access to the share or folder location
+*  Description: Loops through MaintenanceIndexes table and runs a command to
+*  either rebuild or orginize indexes based on on fragmentation percentage
+*  History:
+*  Date:		    Action:								Developer: 
+*  2017-12-21	  Initial version			  Patrick Lee
+******************************************************************************/
+AS
+BEGIN
+-- SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
+ 
+EXEC ('BACKUP DATABASE ['+ @DBName +'] TO  DISK = N'''+ @FileLocation + 
+        @BackupFile +''' WITH NOFORMAT, INIT, COPY_ONLY,  NAME = N'''+ @DBName +'-Full Database Backup'', SKIP, NOREWIND, NOUNLOAD, COMPRESSION,  STATS = 10')
+END
