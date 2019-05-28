d2642d1096197e54598a7dabf2566e2dac8eae05
hyphen-studios <45641620+hyphen-studios@users.noreply.github.com>
2019-05-28 15:01:35 -0700

    Update sp_DatabaseRestore.sql
    Added a debug feature, and resolved a bug.

--- sp_DatabaseRestore.sql
+++ sp_DatabaseRestore.sql
@@ -8,7 +8,8 @@ AS
 *  Description: Restores the database
 *  History:
 *  Date:		    Action:								Developer: 
-*  2017-12-21	  Initial version			  Patrick Lee
+*  2017-12-21		Initial version						Patrick Lee
+*  2019-05-23		Update to allow debug				Patrick Lee
 ******************************************************************************/
 SET NOCOUNT ON
 DECLARE
@@ -30,69 +31,101 @@ DECLARE
 	@dbdatafiles NVARCHAR(max) = ''
 
 /* ---------- Ge information from the backup file ---------- */
-DECLARE @Table TABLE (LogicalName varchar(128),[PhysicalName] varchar(128), [Type] varchar, [FileGroupName] varchar(128), [Size] varchar(128), 
-            [MaxSize] varchar(128), [FileId]varchar(128), [CreateLSN]varchar(128), [DropLSN]varchar(128), [UniqueId]varchar(128), [ReadOnlyLSN]varchar(128), [ReadWriteLSN]varchar(128), 
-            [BackupSizeInBytes]varchar(128), [SourceBlockSize]varchar(128), [FileGroupId]varchar(128), [LogGroupGUID]varchar(128), [DifferentialBaseLSN]varchar(128), [DifferentialBaseGUID]varchar(128), [IsReadOnly]varchar(128), [IsPresent]varchar(128), [TDEThumbprint]varchar(128)
+DECLARE @Table TABLE 
+(
+	LogicalName varchar(128),
+	[PhysicalName] varchar(128),
+	[Type] varchar,
+	[FileGroupName] varchar(128),
+	[Size] varchar(128),
+	[MaxSize] varchar(128),
+	[FileId] varchar(128),
+	[CreateLSN] varchar(128),
+	[DropLSN] varchar(128),
+	[UniqueId] varchar(128),
+	[ReadOnlyLSN] varchar(128),
+	[ReadWriteLSN] varchar(128),
+	[BackupSizeInBytes] varchar(128),
+	[SourceBlockSize] varchar(128),
+	[FileGroupId] varchar(128),
+	[LogGroupGUID] varchar(128),
+	[DifferentialBaseLSN] varchar(128),
+	[DifferentialBaseGUID] varchar(128),
+	[IsReadOnly] varchar(128),
+	[IsPresent] varchar(128),
+	[TDEThumbprint] varchar(128)
 )
 
 INSERT INTO @Table
 	EXEC('RESTORE FILELISTONLY FROM DISK=''' +@FileLocation+'/'+@BackupFile+ '''')
 	--SELECT * FROM @Table
 	SELECT @dbfilecount = count(*) FROM @Table
-	Set @dbfileend = (Select Max(FileId) From @Table);
+	SET @dbfileend = (Select Max(FileId) From @Table);
 	SELECT @backupdatalogicalname = LogicalName FROM @Table WHERE Type = 'D'
 	SELECT @backuploglogicalname = LogicalName FROM @Table WHERE Type = 'L'
+
+IF @Debug = 1
+BEGIN
+	SELECT * FROM @Table
+END
 
 
 BEGIN TRY
 /* ---------- Check to see if the databases exists. If not Restore to a new database ---------- */
-IF OBJECT_ID('tempdb..##dbexists' , 'U') IS NOT NULL
+IF OBJECT_ID('tempdb..##dbexists', 'U') IS NOT NULL
    drop TABLE ##dbexists
 
-SET @SQL =N'SELECT * into ##dbexists FROM '+@DBName+'.sys.database_files;'
+SET @SQL =N'SELECT * INTO ##dbexists FROM '+@DBName+'.sys.database_files;'
 EXEC sys.sp_executesql @SQL
 
 SET @dbexist = (SELECT COUNT(*) FROM ##dbexists)
 
 IF @dbexist >=1
-	BEGIN
-		/* ---------- Get File info ---------- */
-		select @datafile = physical_name, @datafilename = name FROM ##dbexists WHERE file_id = 1;
-		select @logfile = physical_name, @logfilename = name FROM ##dbexists WHERE file_id = 2;
-		-- Get the total number of file configurations for the database
-		SELECT @dbfileend = MAX(file_id) FROM ##dbexists
+BEGIN
+	/* ---------- Get File info ---------- */
+	SELECT @datafile = physical_name, @datafilename = name FROM ##dbexists WHERE file_id = 1;
+	SELECT @logfile = physical_name, @logfilename = name FROM ##dbexists WHERE file_id = 2;
+	-- Get the total number of file configurations for the database
+	SELECT @dbfileend = MAX(file_id) FROM ##dbexists
 
-		IF @dbexist = 2
-			BEGIN				
-				/* ----------  Prep dynamic SQL for the restore code --------- */
-				SET @SQL = N'ALTER DATABASE ['+@DBName+'] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-RESTORE DATABASE ['+@DBName+'] 
-	FROM DISK=N'''+@FileLocation++@BackupFile+''' WITH  FILE = 1,
-	MOVE '''+@datafilename+''' TO '''+@datafile+''',
-	MOVE '''+@logfilename+''' TO '''+@logfile+''',
-	NOUNLOAD,  REPLACE,  STATS = 5					 
-'
-				EXEC sp_executesql @SQL
-			END
-		ELSE			
-			BEGIN
-			WHILE @dbfilestart <= @dbfileend
-			BEGIN
-				SET @dbdatafiles=@dbdatafiles+(SELECT ',MOVE '''+name+''' TO '''+physical_name+'''' FROM ##dbexists WHERE file_id=@dbfilestart)
-				SET @dbfilestart += 1
-			END
+	IF @dbexist = 2
+		BEGIN				
+			/* ----------  Prep dynamic SQL for the restore code --------- */
 			SET @SQL = N'ALTER DATABASE ['+@DBName+'] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
 RESTORE DATABASE ['+@DBName+'] 
-	FROM DISK=N'''+@FileLocation++@BackupFile+''' WITH  FILE = 1,
-	MOVE '''+@datafilename+''' TO '''+@datafile+'''
-	'+@dbdatafiles+',
-	MOVE '''+@logfilename+''' TO '''+@logfile+''',
-	NOUNLOAD,  REPLACE,  STATS = 5					 
+FROM DISK=N'''+@FileLocation++@BackupFile+''' WITH  FILE = 1,
+MOVE '''+@backupdatalogicalname+''' TO '''+@datafile+''',
+MOVE '''+@backuploglogicalname+''' TO '''+@logfile+''', NOUNLOAD,  REPLACE,  STATS = 5					 
 '
-			EXEC sp_executesql @SQL	
-			END
+			IF @Debug = 0
+				EXEC sp_executesql @SQL
+			ELSE
+				PRINT '1'
+				PRINT @SQL
+		END
+	ELSE			
+		BEGIN
+		WHILE @dbfilestart <= @dbfileend
+		BEGIN
+			SET @dbdatafiles=@dbdatafiles+(SELECT ',MOVE '''+name+''' TO '''+physical_name+'''' FROM ##dbexists WHERE file_id=@dbfilestart)
+			SET @dbfilestart += 1
+		END
+		SET @SQL = N'ALTER DATABASE ['+@DBName+'] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+RESTORE DATABASE ['+@DBName+'] 
+FROM DISK=N'''+@FileLocation++@BackupFile+''' WITH  FILE = 1,
+MOVE '''+@datafilename+''' TO '''+@datafile+'''
+'+@dbdatafiles+',
+MOVE '''+@logfilename+''' TO '''+@logfile+''',
+NOUNLOAD,  REPLACE,  STATS = 5					 
+'
+		IF @Debug = 0
+			EXEC sp_executesql @SQL
+		ELSE
+			PRINT '2'
+			PRINT @SQL	
+		END
 			
-	END	
+END
 END TRY
 BEGIN CATCH
 	/* ---------- Get full count to prep for left() statement ---------- */
@@ -101,14 +134,12 @@ BEGIN CATCH
 		INNER JOIN sys.[databases] d   
 			ON mf.[database_id] = d.[database_id]   
     WHERE d.[name] = @dbname AND type = 0);
-
 	--Get the default Log path  
 	SELECT @defaultlogfilelocaiton=(SELECT LEFT(physical_name,LEN(physical_name)-CHARINDEX('\',REVERSE(physical_name))+1)   
-		FROM sys.master_files mf   
-			INNER JOIN sys.[databases] d   
-				ON mf.[database_id] = d.[database_id]   
-		WHERE d.[name] = @dbname AND type = 1);
-
+	FROM sys.master_files mf   
+		INNER JOIN sys.[databases] d   
+			ON mf.[database_id] = d.[database_id]   
+	WHERE d.[name] = @dbname AND type = 1);
 	--/* ---------- Start to build the files for restore ----------*/
 	IF @dbfilecount >= 3
 	BEGIN
@@ -132,25 +163,50 @@ FROM DISK=N'''+@FileLocation+'/'+@BackupFile+''' WITH FILE=1,
 	'+@dbdatafiles+';
 
 '
-
-	EXEC sp_executesql @SQL
+	IF @Debug = 0
+		EXEC sp_executesql @SQL
+	ELSE
+		PRINT '3'
+		PRINT @SQL
     
 END CATCH
+-- Set proper logical names for the database
+SET @SQL=N'USE [master]; ALTER DATABASE ['+@DBName+'] MODIFY FILE ( NAME = '+@backupdatalogicalname+', NEWNAME = '+@DBName+' );'
+IF @Debug = 0
+	EXEC sp_executesql @SQL
+ELSE
+	PRINT '4'
+	PRINT @SQL
+
+SET @SQL=N'USE [master]; ALTER DATABASE ['+@DBName+'] MODIFY FILE ( NAME = '+@backuploglogicalname+', NEWNAME = '+@DBName+'_log );'
+IF @Debug = 0
+	EXEC sp_executesql @SQL
+ELSE
+	PRINT '5'
+	PRINT @SQL
 
 -- Set database to simple recovery model
 IF @SetToSimple = 1
-	BEGIN
-		SET @SQL= 'USE Master; ALTER DATABASE ['+ @DBName +'] SET RECOVERY SIMPLE WITH NO_WAIT;'
-		EXEC sp_executesql @SQL		
+BEGIN
+	SET @SQL= 'USE Master; ALTER DATABASE ['+ @DBName +'] SET RECOVERY SIMPLE WITH NO_WAIT;'
 		
-	END
+	IF @Debug = 0
+		EXEC sp_executesql @SQL
+	ELSE
+		PRINT '6'
+		PRINT @SQL
+		
+END
 
 -- Shring the log file to 64 megs
 IF @ShrinkLog = 1
-	BEGIN
-		SET @SQL = 'USE ['+ @DBName+']
-		DBCC SHRINKFILE (N'+ @logfilename +' , 64)
-		'
+BEGIN
+	SET @SQL = 'USE ['+@DBName+']
+	DBCC SHRINKFILE ('+@DBName+'_log , 64)
+	'
+	IF @Debug = 0
 		EXEC sp_executesql @SQL
-
-	END 
+	ELSE
+		PRINT '7'
+		PRINT @SQL
+END 
